d4d3093ef7e7af7a4806987f830843ecfefc2b7f
Rijul Saxena <rijul.saxena@gmail.com>
2023-02-16 14:47:07 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "Appending for webhook\n"
+  cout << "Appending for webhook\n";
   return 0;
 }
