4976275de0f0e64526882c478c45c5dcdf2f7807
Rijul Saxena <rijul.saxena@gmail.com>
2023-02-16 15:00:07 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World-2\n";
+  cout << "Hello, Jenkins-2\n";
+  cout << "I have successfully built and run-2\n";
+  return 0;
+}
